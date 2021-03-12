39207ec99e73946d9236b7177d69b028ed0cc1a9
vishalgaddam873 <vishal18@navgurukul.org>
2021-03-12 12:56:56 +0530

    Final Output Images added

--- js/addButtons.js
+++ js/addButtons.js
@@ -4,13 +4,13 @@ AFRAME.registerComponent("createbuttons", {
     var button1 = document.createElement("button");
     button1.innerHTML = "ORDER NOW";
     button1.setAttribute("id", "order-button");
-    button1.setAttribute("class", "btn btn-warning ml-3 mr-3");
+    button1.setAttribute("class", "btn btn-danger ml-3 mr-3");
 
     // 2. Create the Bill button
     var button2 = document.createElement("button");
     button2.innerHTML = "ORDER SUMMARY";
     button2.setAttribute("id", "order-summary-button");
-    button2.setAttribute("class", "btn btn-warning ml-3");
+    button2.setAttribute("class", "btn btn-danger ml-3");
 
     // 3. Append somewhere
     var buttonDiv = document.getElementById("button-div");
